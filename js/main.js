ba18927a86e540175a4abdc3d4d7530f73824cca
Krypto1337 <118254306+Krypto1337@users.noreply.github.com>
2022-11-21 08:30:27 +0100

    Created project structure, added Books-Constructor

---
+++ js/main.js
@@ -0,0 +1,14 @@
+let myLibrary = [];
+
+function Books(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+function addBookToLibrary() {
+  myLibrary.forEach((element) => {
+    console.log(element);
+  });
+}

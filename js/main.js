f345281f75b4185cd5cf9042e0d9881fdd72348c
Rene B. <133756710+Krypto1337@users.noreply.github.com>
2024-07-27 15:51:40 +0200

    Added function to delete books, but many bugs left

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
+//Declerations
 const myLibrary = [];
+const container = document.querySelector(".container");
+const inputTitle = document.querySelector(".inputTitle");
+const inputAuthor = document.querySelector(".inputAuthor");
+const inputPages = document.querySelector(".inputPages");
+const radioRead = document.getElementById("radio-read");
+const radioNotRead = document.getElementById("radio-not-read");
+const form = document.getElementById("addBookForm");
+const formBtn = document.querySelector(".btn-primary");
+const btnAddBook = document.querySelector(".btnBook");
 
 function Book(title, author, pages, read) {
 	this.title = title;
@@ -10,49 +20,56 @@ function Book(title, author, pages, read) {
 	};
 }
 
-addBookToLibrary("The Hobbit", "J.R.R. Tolkien", "295", "Read");
-addBookToLibrary("The Lord of the Rings", "J.R.R. Tolkien", "1216", "Not read");
-addBookToLibrary("The Hobbit", "J.R.R. Tolkien", "295", "Not read");
-addBookToLibrary("The Hobbit", "J.R.R. Tolkien", "295", "Read");
-
 function addBookToLibrary(title, author, pages, read) {
 	myLibrary.push(new Book(title, author, pages, read));
 }
-const container = document.querySelector(".container");
-let i = 0;
+
 function displayBook(library) {
-	library.forEach((element) => {
+	library.forEach((element, index) => {
 		const card = document.createElement("div");
+		const btnContainer = document.createElement("div");
+		btnContainer.className = "btnCardContainer";
 		const pTitle = document.createElement("p");
 		const pAuthor = document.createElement("p");
 		const pPages = document.createElement("p");
 		const btnToggleRead = document.createElement("button");
+		const btnRemoveBook = document.createElement("button");
+
 		card.id = "card";
-		card.className = i;
-		i++;
+		card.dataset.index = index;
 		container.append(card);
+
 		pTitle.textContent = `Title: ${element.title}`;
 		card.appendChild(pTitle);
+
 		pAuthor.textContent = `Author: ${element.author}`;
 		card.appendChild(pAuthor);
+
 		pPages.textContent = `Pages: ${element.pages}`;
 		card.appendChild(pPages);
-		btnToggleRead.className = "btn";
+		card.appendChild(btnContainer);
+
+		btnToggleRead.classList.add("btn", "btn-secondary", "btn-toggle-read");
 		btnToggleRead.textContent = element.read;
 		btnToggleRead.addEventListener("click", () => {
-			if (btnToggleRead.textContent == "Read") {
+			if (btnToggleRead.textContent === "Read") {
 				btnToggleRead.textContent = "Not read";
 			} else {
 				btnToggleRead.textContent = "Read";
 			}
 		});
-		card.appendChild(btnToggleRead);
+		btnRemoveBook.classList.add("btn", "btn-secondary", "btn-remove-book");
+		btnRemoveBook.textContent = "Remove";
+		btnRemoveBook.addEventListener("click", (e) => {
+			const index = e.target.closest(".btn-remove-book").dataset.index;
+			myLibrary.splice(index, 1);
+			displayBook(myLibrary);
+		});
+		btnContainer.appendChild(btnToggleRead);
+		btnContainer.appendChild(btnRemoveBook);
 	});
 }
 
-displayBook(myLibrary);
-
-const btnAddBook = document.querySelector(".btnBook");
 btnAddBook.addEventListener("click", () => {
 	document.querySelector(".form-popup").style.display = "block";
 	document.querySelector(".btnDiv").style.display = "none";
@@ -62,18 +79,10 @@ btnAddBook.addEventListener("click", () => {
 //Form functionality
 
 //Prevent form-submit
-const form = document.getElementById("addBookForm");
 function handleForm(event) {
 	event.preventDefault();
 }
 form.addEventListener("submit", handleForm);
-
-const inputTitle = document.querySelector(".inputTitle");
-const inputAuthor = document.querySelector(".inputAuthor");
-const inputPages = document.querySelector(".inputPages");
-const radioRead = document.getElementById("radio-read");
-const radioNotRead = document.getElementById("radio-not-read");
-const formBtn = document.querySelector(".btn-primary");
 
 formBtn.addEventListener("click", () => {
 	if (radioRead.checked) {
@@ -96,3 +105,10 @@ formBtn.addEventListener("click", () => {
 	document.querySelector(".btnDiv").style.display = "flex";
 	container.style.display = "grid";
 });
+
+//Default Books
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", "295", "Read");
+addBookToLibrary("The Lord of the Rings", "J.R.R. Tolkien", "1216", "Not read");
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", "295", "Not read");
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", "295", "Read");
+displayBook(myLibrary);
